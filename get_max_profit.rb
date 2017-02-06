469bb8e93925be8be902a045e58a30874183c058
Katrina Tollin <ktollin@gmail.com>
2017-02-06 14:27:19 -0600

    add get_max_profit file

---
+++ get_max_profit.rb
@@ -0,0 +1,10 @@
+# source: interviewcake.com
+
+stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
+
+def get_max_profit(array)
+
+end
+
+get_max_profit(stock_prices_yesterday)
+# returns 6 (buying for $5 and selling for $11)(array)
