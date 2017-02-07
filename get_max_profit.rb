40923259f742d9142651117b2e55ef13ec10e7ad
Katrina Tollin <ktollin@gmail.com>
2017-02-07 17:32:12 -0600

    comment

--- get_max_profit.rb
+++ get_max_profit.rb
@@ -3,8 +3,15 @@
 stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 
 def get_max_profit(array)
-
+  hash = {}
+  for array.each_index do |i|
+    for array.each do |x|
+      hash[x] = array[i] - array[x]
+    end
+  end
 end
 
 get_max_profit(stock_prices_yesterday)
 # returns 6 (buying for $5 and selling for $11)(array)
+
+
