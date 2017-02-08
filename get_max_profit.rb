016f0b9a2dcb476d8d7228e902572b7f193b079c
Katrina Tollin <ktollin@gmail.com>
2017-02-08 13:09:47 -0600

    remove old solution

--- get_max_profit.rb
+++ get_max_profit.rb
@@ -2,14 +2,7 @@
 
 stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 
-# def get_max_profit(array)
-#   hash = {}
-#   for array.each_index do |i|
-#     for array.each do |x|
-#       hash[x] = array[i] - array[x]
-#     end
-#   end
-# end
+
 
 def get_max_profit(array)
    diff = 0
